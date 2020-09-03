2adaaf21313c4d3ea52ddf0112e32c2116ed1d3a
bogdan <bogdantrach228@gmail.com>
2020-09-03 21:48:05 +0300

    telegramm bot

---
+++ bot.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+
+const Telegraf = require('telegraf');
+const api = require('covid19-api');
+let Markup = require('telegraf/markup');
+const countryName = require('./contries');
+
+const bot = new Telegraf(process.env.bot_TOKEN);
+
+bot.start((ctx) =>
+  ctx.reply(
+    `Привет, ${ctx.message.from.first_name}!
+
+`,
+    Markup.keyboard([
+      ['Ukraine', 'Russia'],
+      ['/help', '/start'],
+    ])
+      .resize()
+      .extra()
+  )
+);
+
+bot.help((ctx) => ctx.reply('Здесь можешь просмотреть список доступных стран ' + countryName));
+bot.on('sticker', async (ctx) =>
+  ctx.reply('Мне очень нравится это стикер, но нужно названия страны  ,можешь воспользоватся /help')
+);
+
+bot.on('text', async (ctx) => {
+  let data = {};
+  try {
+    data = await api.getReportsByCountries(ctx.message.text);
+
+    const FormatData = `
+Страна:${data[0][0].country}
+Случаи:${data[0][0].cases}
+Смеpти:${data[0][0].deaths}
+Вылечено:${data[0][0].recovered}
+  `;
+    ctx.reply(FormatData);
+  } catch {
+    console.log('ERROR');
+    ctx.reply(`${ctx.message.from.first_name}, название страны указано неверно ! `);
+  }
+});
+
+bot.hears('Спасибо', (ctx) => ctx.reply('Всегда пожалуйста)'));
+bot.launch();
+
+// this is the console log
+console.log('Bot started');
---
+++ node_modules/covid19-api/test/app.test.js
@@ -0,0 +1,77 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { expect , assert } = chai;
+
+const URL = 'https://covid19-server.chrismichael.now.sh/api/v1';
+
+chai.use(chaiHttp)
+
+describe('COVID19 API' , () =>{
+  it('GET METHOD /api/v1/AllReports' , (ok) =>{
+    chai
+      .request(URL)
+      .get('/AllReports')
+      .end((err , res) =>{      
+        expect(res.status).to.be.equal(200);
+        expect(res.body.reports).to.be.an('array');
+        
+        const table = res.body.reports[0].table;
+        const tableKeys = [
+          "TotalCases"       , "NewCases",
+          "TotalDeaths"      , "NewDeaths",
+          "TotalRecovered"   , "ActiveCases",
+          "Deaths_1M_pop"    , "Country",
+          "Serious_Critical" , "Tests_1M_Pop",
+          "TotCases_1M_Pop"  , "TotalTests",
+          "Continent"
+        ]
+
+        Array.from({length: table.length} , (v , i) =>{
+          Array.from({length: table[i].length} , (v , j) =>{
+            expect(table[i][j]).to.have.keys(tableKeys)
+          });
+        });
+        ok();
+      })
+  }).timeout(10000);
+
+  it('GET METHOD /api/v1/Deaths' , (ok) =>{
+    chai
+      .request(URL)
+      .get('/Deaths')
+      .end((err , res) =>{      
+        expect(res.status).to.be.equal(200);
+        expect(res.body.deaths).to.be.an('Array');
+
+        const tableKeys = [
+          "Date" ,          "TotalDeaths",
+          "ChangeInTotal" , "ChangeTotalInPercent"
+        ]
+
+        expect(res.body.deaths[0]).to.have.keys(tableKeys);
+        ok();
+      })
+  }).timeout(10000);
+
+  
+  it('GET METHOD /api/v1/JohnsHopkinsDataDailyReport' , (ok) =>{
+    chai
+      .request(URL)
+      .get('/JohnsHopkinsDataDailyReport')
+      .end((err , res) =>{      
+        expect(res.status).to.be.equal(200);
+        expect(res.body.data).to.be.an('object');
+
+        const tableKeys = [
+          "Province_State" ,  "Country_Region",
+          "Last_Update"    ,  "Lat",
+          "Long_"          ,  "Confirmed",
+          "Deaths"         ,  "Recovered",
+          "Active"         ,  "Combined_Key"
+        ]
+
+        expect(res.body.data.table[0]).to.have.keys(tableKeys);
+        ok();
+      })
+  }).timeout(10000);
+})
